9f0c29b3b50486dc7435bf9cbebbced3122d63bf
maasai <jkemboe@gmail.com>
2019-09-21 16:08:31 +0300

    Ready for deployemnt

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'index.html'));
+})
+
+app.listen(port)
