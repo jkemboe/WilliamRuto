99af26a6606120ceb619a5d104a58983839e3dc6
maasai <jkemboe@gmail.com>
2019-12-10 17:05:38 +0300

    GoogleAnalytics Tracker implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+ import ReactGA from 'react-ga';
 
 import Main from './components/main/main';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('UA-47727623-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  },[]);
   return (
        <div>
          <Main/>
